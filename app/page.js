586a9091d7048ff9cc21355c9f121beabd198e19
Hamza <hamzaamir7651@gmail.com>
2025-10-10 19:17:49 +0500

    finalized landing page

--- app/page.js
+++ app/page.js
@@ -2,102 +2,74 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex flex-col items-center justify-center h-[45vh] text-white">
+        <div className="text-3xl flex justify-center items-center mt-6 font-bold">Buy me a Coffee <span><img className="invert-100" src="/coffee.gif" width={50} alt="" /></span></div>
+        <p className="text-lg mt-3">
+          A crowdfunding platform for creator. Get funded by your fans and followers. Start now!
+        </p>
+        <div className="flex mt-5">
+          <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Start Here
+          </button>
+          <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Read More
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="bg-white h-1 opacity-15"></div>
+
+      <section className="text-white py-16 container mx-auto">
+        <h1 className="text-lg text-center mb-5 font-bold">
+          Your fans can buy you a coffee to support your projects!
+        </h1>
+        <div className="flex gap-5 justify-around">
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/developer.png" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/coin.gif" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/developer.png" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+        </div>
+      </section>
+
+      <div className="bg-white h-1 opacity-15 mt-5"></div>
+
+      <section className="text-white container py-16 mx-auto">
+        <h1 className="text-lg text-center mb-5 font-bold">
+          Learn more about us
+        </h1>
+        <div className="flex gap-5 justify-around">
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/developer.png" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/coin.gif" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+
+          <div className="space-y-3 flex flex-col justify-between items-center">
+            <img className="p-2 bg-gray-500 rounded-full" src="/developer.png" width={88} alt="" />
+            <p>Fans want to help you</p>
+            <p className="text-center">Your fans are available for you to buy a coffee</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
